52a95aae444b313f2aaf96993e1877aebbcf7e9f
GMG-bit <gas.mardones@duocuc.cl>
2026-06-22 15:52:00 -0400

    test: provocar fallo controlado de pruebas unitarias

--- test.js
+++ test.js
@@ -19,6 +19,10 @@ const result = spawnSync(process.execPath, [jestBin, '--runInBand', ...passthrou
   env: process.env,
 });
 
+test('Forzar fallo de test en EA3', () => {
+  expect(true).toBe(false);
+});
+
 if (result.error) {
   console.error(result.error.message);
   process.exit(1);
